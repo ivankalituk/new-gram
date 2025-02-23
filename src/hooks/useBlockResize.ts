137889f58d3443fdc8869c907a93424b8e2a9961
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-02-24 01:46:43 +0200

    resize hook fix

--- src/hooks/useBlockResize.ts
+++ src/hooks/useBlockResize.ts
@@ -1,43 +1,51 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useBlockResize = (minSize: number, maxSize: number, initialSize: number, invert?: boolean) => {
+export const useBlockResize = (
+  minSize: number,
+  maxSize: number,
+  initialSize: number,
+  invert?: boolean
+) => {
+  invert = invert ?? false;
+  const [blockSize, setBlockSize] = useState<number>(initialSize);
+  const [isBlockResizing, setIsBlockResizing] = useState<boolean>(false);
 
-    invert = invert ?? false;
-    const [blockSize, setBlockSize] = useState<number>(initialSize)
-    const [isBlockResizing, setIsBlockResizing] = useState<boolean>(false)
+  const handleMouseDown = () => {
+    setIsBlockResizing(true);
+    // Запрещаем выделение текста
+    document.body.style.userSelect = "none";
+  };
 
-    const handleMouseDown = () => {
-        setIsBlockResizing(true)
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isBlockResizing) return;
+    setBlockSize((prev) => {
+      let newWidth;
+      if (invert) {
+        newWidth = prev - e.movementX;
+      } else {
+        newWidth = prev + e.movementX;
+      }
+      return Math.min(Math.max(newWidth, minSize), maxSize);
+    });
+  };
+
+  const handleMouseUp = () => {
+    setIsBlockResizing(false);
+    // Включаем выделение текста обратно
+    document.body.style.userSelect = "auto";
+  };
+
+  useEffect(() => {
+    if (isBlockResizing) {
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+    }
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
     };
+  }, [isBlockResizing]);
 
-    const handleMouseMove = (e: MouseEvent) => {
-        if (!isBlockResizing) return
-        setBlockSize((prev) => {
-            let newWidth
-            if (invert){
-                newWidth = prev - e.movementX;
-            } else {
-                newWidth = prev + e.movementX;
-            }
-            return Math.min(Math.max(newWidth, minSize), maxSize);
-        });
-    }
-
-    const handleMouseUp = () => {
-        setIsBlockResizing(false)
-    }
-
-    useEffect(() => {
-        if (isBlockResizing) {
-            document.addEventListener("mousemove", handleMouseMove)
-            document.addEventListener("mouseup", handleMouseUp)
-        }
-
-        return () => {
-            document.removeEventListener("mousemove", handleMouseMove)
-            document.removeEventListener("mouseup", handleMouseUp)
-        }
-    }, [isBlockResizing])
-
-    return {blockSize, handleMouseDown};
-}
+  return { blockSize, handleMouseDown };
+};
