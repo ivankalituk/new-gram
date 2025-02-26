aba0f8d2d769f191e150c6bb555d0c6ab4a168d0
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-02-26 03:51:12 +0200

    adaptive main changes

--- src/pages/chatPage/chatPage.tsx
+++ src/pages/chatPage/chatPage.tsx
@@ -1,22 +1,26 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import './chatPage.scss'
 import Chats from "../../components/chatPage/chats/chats";
 import Conversation from "../../components/chatPage/conversation/conversation";
 import { useParams } from "react-router-dom";
-import ModalMessage from "../../shared/modalMessage/modalMessage";
 
 const ChatPage: FC = () => {
 
     const {chatId} = useParams()
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
 
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= 768)
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    })
     
     return(
         <div className="chatPage">
-            <Chats />
+            {(!chatId || !isMobile) && <Chats />}
 
             {chatId && <Conversation />}
 
-            <ModalMessage />
         </div>
     )
 }
